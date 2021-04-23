2ffb82d05e8f8b10fa44fc076779295c23a0bcdd
Vladyslav <vladtravel2015@gmail.com>
2021-04-23 17:02:52 +0300

    исправление

--- gallery-items.js
+++ gallery-items.js
@@ -1,4 +1,4 @@
-const gallery = [
+export default [
   {
     preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
@@ -45,50 +45,3 @@ const gallery = [
     description: 'Lighthouse Coast Sea',
   },
 ];
-
-const galleryContainer = document.querySelector('.js-gallery');
-
-const lightboxImage = document.querySelector('.lightbox__image');
-
-const lightboxAddIsopen = document.querySelector('.js-lightbox');
-
-const btnClose = document.querySelector('.lightbox__button');
-
-btnClose.addEventListener('click', onBtnClick);
-
-const imgMark = imgMarkup(gallery);
-
-galleryContainer.insertAdjacentHTML('beforeend', imgMark);
-
-galleryContainer.addEventListener('click', onImageClick);
-
-function imgMarkup(gallery) {
-  return gallery
-    .map(({ preview, description }) => {
-      return `<li class="gallery__item">
-        <a class="gallery__link" href="">
-          <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-      </li>`;
-    })
-    .join('');
-}
-
-function onImageClick(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-  lightboxAddIsopen.classList.add('is-open');
-
-  gallery.map(({ description, original }) => {
-    if (evt.target.alt === description) {
-      lightboxImage.src = `${original}`;
-    }
-  });
-}
-
-function onBtnClick(evt) {
-  lightboxAddIsopen.classList.remove('is-open');
-  lightboxImage.src = '';
-}
---
+++ task-8.js
@@ -0,0 +1,49 @@
+import gallery from './gallery-items.js';
+console.log(gallery);
+
+const galleryContainer = document.querySelector('.js-gallery');
+
+const lightboxImage = document.querySelector('.lightbox__image');
+
+const lightboxAddIsopen = document.querySelector('.js-lightbox');
+
+const btnClose = document.querySelector('.lightbox__button');
+
+btnClose.addEventListener('click', onBtnClick);
+
+const imgMark = imgMarkup(gallery);
+
+galleryContainer.insertAdjacentHTML('beforeend', imgMark);
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function imgMarkup(gallery) {
+  return gallery
+    .map(({ preview, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  lightboxAddIsopen.classList.add('is-open');
+
+  gallery.map(({ description, original }) => {
+    if (evt.target.alt === description) {
+      lightboxImage.src = `${original}`;
+    }
+  });
+}
+
+function onBtnClick(evt) {
+  lightboxAddIsopen.classList.remove('is-open');
+  lightboxImage.src = '';
+}
